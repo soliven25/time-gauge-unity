8351c844d82b099410ef91a0342364df9eeaf1b4
Sungkeun Choi <soliven25@gmail.com>
2016-08-12 15:13:27 +0900

    first commit for time gauge via unity UI

---
+++ Assets/Scripts/TimeGaugeCtrl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeGaugeCtrl : MonoBehaviour {
+
+	private Image timeGauge;
+	public float speed = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+		timeGauge = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeGauge.fillAmount -= speed;
+	
+	}
+}
